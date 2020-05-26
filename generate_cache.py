f0bac5fa9a6072f747730f90e63118a422fcbee9
Aidan <ccaidand@gmail.com>
2020-05-26 02:22:12 -0400

    Updated README, added cache generator script

---
+++ generate_cache.py
@@ -0,0 +1,13 @@
+import spotipy
+import spotipy.util as util
+
+if __name__ == '__main__':
+    with open("app_info.txt") as app_info:
+        username = input("Enter username: ")
+        client_id = app_info.readline()
+        client_secret = app_info.readline()
+        redirect_uri = app_info.readline()
+        scope = "playlist-modify-private playlist-modify-public user-follow-read ugc-image-upload"
+
+        token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+        sp = spotipy.Spotify(token)
